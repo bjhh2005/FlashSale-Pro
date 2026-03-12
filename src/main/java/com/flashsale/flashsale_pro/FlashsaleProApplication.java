97a70969aedecf31581c9d3b8939c6af44710ec6
bjhh2005 <2467366054@qq.com>
2026-03-12 22:28:53 +0800

    <init>: spring初始化

---
+++ src/main/java/com/flashsale/flashsale_pro/FlashsaleProApplication.java
@@ -0,0 +1,13 @@
+package com.flashsale.flashsale_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashsaleProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashsaleProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flashsale/flashsale_pro/FlashsaleProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashsale.flashsale_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashsaleProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
